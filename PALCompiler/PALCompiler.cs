efb1f22ce287aa787008405018e0ef446bca2bc8
7138gmhampton <0407435@abertay.ac.uk>
2021-02-27 15:41:03 +0000

    Create simple output of generated tokens

---
+++ PALCompiler/PALCompiler.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AllanMilne.Ardkit;
+
+namespace PALCompiler
+{
+    class PALCompiler
+    {
+        static void Main(string[] args)
+        {
+            string source_file = (args.Length == 1) ? args[0] : inputSourceFile();
+
+            var tokens = new List<IToken>();
+            var errors = new List<ICompilerError>();
+
+            var scanner = new PALScanner();
+            scanner.Init(new StreamReader(source_file), errors);
+
+            while (!scanner.EndOfFile) tokens.Add(scanner.NextToken());
+
+            if (errors.Count != 0)
+                foreach (var error in errors)
+                    Console.WriteLine(error.ToString());
+            else {
+                using (var output_file = new StreamWriter("output.txt", false)) {
+                    foreach (var token in tokens) output_file.WriteLine(token.ToString());
+                }
+            }
+        }
+
+        private static string inputSourceFile()
+        {
+            while (true) {
+                Console.Write("Source file: ");
+                string source_file = Console.ReadLine();
+
+                if (File.Exists(source_file)) return source_file;
+                else Console.WriteLine("Invalid file - try again");
+            }
+        }
+    }
+}
