8685b8035b650a24174fdaa1d327a229ae9a46dd
7138gmhampton <0407435@abertay.ac.uk>
2021-02-27 21:02:10 +0000

    Create parser class

---
+++ PALCompiler/PALParser.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AllanMilne.Ardkit;
+
+namespace PALCompiler
+{
+    class PALParser : RdParser
+    {
+        PALParser(IScanner scanner) : base(scanner) { }
+
+        protected override void recStarter() => throw new NotImplementedException();
+    }
+}
