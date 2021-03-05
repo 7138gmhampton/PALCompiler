c8939081633204c6ee731b44ceebf217d3008e18
7138gmhampton <0407435@abertay.ac.uk>
2021-03-05 12:11:04 +0000

    Create skeleton o fartifact generator class

---
+++ PALCompiler/PALCSGenerator.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PALCompiler
+{
+    class PALCSGenerator
+    {
+        private delegate void Generator();
+
+        private static Dictionary<string, Generator> symbol_to_generator =
+            new Dictionary<string, Generator>
+            {
+                { "<Program>", Generators.generateProgram }
+            };
+
+        internal PALCSGenerator() { }
+
+        internal void generate(TextWriter output)
+        {
+            throw new NotImplementedException();
+        }
+
+        private static class Generators
+        {
+            internal static void generateProgram() => throw new NotImplementedException();
+        }
+    }
+}
