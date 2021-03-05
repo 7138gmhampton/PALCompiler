f25a504fb67e3f8204b969f7d604121e34a6a3db
7138gmhampton <0407435@abertay.ac.uk>
2021-03-05 15:19:21 +0000

    Implement class declaration for program generator

--- PALCompiler/PALCSGenerator.cs
+++ PALCompiler/PALCSGenerator.cs
@@ -9,12 +9,12 @@ namespace PALCompiler
 {
     class PALCSGenerator
     {
-        private delegate string Generator();
+        private delegate string Generator(SyntaxNode node);
 
         private readonly static Dictionary<string, Generator> symbol_to_generator =
             new Dictionary<string, Generator>
             {
-                { "<Program>", Generators.generateProgram }
+                { "<Program>", Generators.generateRoot }
             };
 
         internal PALCSGenerator() { }
@@ -22,27 +22,35 @@ namespace PALCompiler
         internal string generate(SyntaxNode syntax_tree)
         {
             //throw new NotImplementedException();
-            //syntax_tree.printGraphic("", true);
+            syntax_tree.printGraphic("", true);
 
             StringBuilder artifact = new StringBuilder();
 
             SyntaxNode cursor = null;
             SyntaxNode precursor = null;
 
+            //artifact.Append(Generators.generateRoot());
+
             cursor = syntax_tree;
             //symbol_to_generator[cursor.Symbol].Invoke();
-            artifact.Append(symbol_to_generator[cursor.Symbol].Invoke());
+            artifact.Append(symbol_to_generator[cursor.Symbol].Invoke(syntax_tree));
+
+
 
             return artifact.ToString();
         }
 
         private static class Generators
         {
-            internal static string generateProgram()
+            internal static string generateRoot(SyntaxNode node)
             {
                 StringBuilder code = new StringBuilder();
 
                 code.Append("using System;\nusing System.IO;\n\n");
+
+                code.Append("class " + node.Children[1].Symbol + "\n{\n");
+
+                code.AppendLine("}");
 
                 return code.ToString();
             }
