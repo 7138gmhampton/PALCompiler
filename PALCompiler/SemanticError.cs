1aa756835e8c19f40f5ed2a62133b56062c1fedb
7138gmhampton <0407435@abertay.ac.uk>
2021-03-12 14:26:00 +0000

    Create compiler error for general semantic errors

---
+++ PALCompiler/SemanticError.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AllanMilne.Ardkit;
+
+namespace PALCompiler
+{
+    class SemanticError : CompilerError
+    {
+        private readonly string message;
+
+        internal SemanticError(IToken token, string message) : base(token)
+        {
+            this.message = message;
+        }
+
+        public override string ToString()
+        {
+            return $"At line {token.Line}, column {token.Column}: {message}";
+        }
+    }
+}
