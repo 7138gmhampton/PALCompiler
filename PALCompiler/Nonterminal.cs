d1bc96d93d29bc2153f31f0aa0c2e55750dd5f46
7138gmhampton <0407435@abertay.ac.uk>
2021-03-12 16:26:17 +0000

    Create nonterminal common source

---
+++ PALCompiler/Nonterminal.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+
+namespace PALCompiler
+{
+    internal static class Nonterminals
+    {
+        public static readonly string ASSIGMENT = "<Assignment>";
+        public static readonly string BOOLEAN_EXPRESSION = "<BooleanExpr>";
+        public static readonly string CONDITIONAL = "<Conditional>";
+        public static readonly string EXPRESSION = "<Expression>";
+        public static readonly string FACTOR = "<Factor>";
+        public static readonly string IDENTIFIER_LIST = "<IdentList>";
+        public static readonly string IO = "<I-o>";
+        public static readonly string LOOP = "<Loop>";
+        public static readonly string STATEMENT = "<Statement>";
+        public static readonly string TERM = "<Term>";
+        public static readonly string TYPE = "<Type>";
+        public static readonly string VALUE = "<Value>";
+        public static readonly string VARIABLE_DECLARATION = "<VarDecls>";
+    }
+}
