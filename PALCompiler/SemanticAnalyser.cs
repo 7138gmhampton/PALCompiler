6291bb9c2b58414a869c9e3ad21b6b6aed88357b
7138gmhampton <0407435@abertay.ac.uk>
2021-03-12 11:56:04 +0000

    Add semantic analyser skeleton

---
+++ PALCompiler/SemanticAnalyser.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AllanMilne.Ardkit;
+
+namespace PALCompiler
+{
+    class SemanticAnalyser
+    {
+        private SymbolTable symbols;
+        private SyntaxNode syntax_tree;
+
+        internal SemanticAnalyser(SyntaxNode syntax_tree, SymbolTable symbols)
+        {
+            
+        }
+    }
+}
