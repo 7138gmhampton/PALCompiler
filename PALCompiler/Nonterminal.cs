86dba3aa6f9c7d01bf30731844c353e387b84f35
7138gmhampton <0407435@abertay.ac.uk>
2021-03-12 16:54:58 +0000

    Replace static readonly fields with constants

--- PALCompiler/Nonterminal.cs
+++ PALCompiler/Nonterminal.cs
@@ -4,18 +4,19 @@ namespace PALCompiler
 {
     internal static class Nonterminals
     {
-        public static readonly string ASSIGMENT = "<Assignment>";
-        public static readonly string BOOLEAN_EXPRESSION = "<BooleanExpr>";
-        public static readonly string CONDITIONAL = "<Conditional>";
-        public static readonly string EXPRESSION = "<Expression>";
-        public static readonly string FACTOR = "<Factor>";
-        public static readonly string IDENTIFIER_LIST = "<IdentList>";
-        public static readonly string IO = "<I-o>";
-        public static readonly string LOOP = "<Loop>";
-        public static readonly string STATEMENT = "<Statement>";
-        public static readonly string TERM = "<Term>";
-        public static readonly string TYPE = "<Type>";
-        public static readonly string VALUE = "<Value>";
-        public static readonly string VARIABLE_DECLARATION = "<VarDecls>";
+        //public static readonly string ASSIGMENT = "<Assignment>";
+        public const string ASSIGMENT = "<Assignment>";
+        public const string BOOLEAN_EXPRESSION = "<BooleanExpr>";
+        public const string CONDITIONAL = "<Conditional>";
+        public const string EXPRESSION = "<Expression>";
+        public const string FACTOR = "<Factor>";
+        public const string IDENTIFIER_LIST = "<IdentList>";
+        public const string IO = "<I-o>";
+        public const string LOOP = "<Loop>";
+        public const string STATEMENT = "<Statement>";
+        public const string TERM = "<Term>";
+        public const string TYPE = "<Type>";
+        public const string VALUE = "<Value>";
+        public const string VARIABLE_DECLARATION = "<VarDecls>";
     }
 }
