cef33bfc6a2fc7092ca4c1551707a0d973daec81
7138gmhampton <0407435@abertay.ac.uk>
2021-03-16 19:27:12 +0000

    Add success message

--- PALCompiler/PALCompiler.cs
+++ PALCompiler/PALCompiler.cs
@@ -24,6 +24,7 @@ namespace PALCompiler
                 foreach (var error in parser.Errors) Console.WriteLine(error.ToString());
             else {
                 generateCSArtifact(source_file, parser.SyntaxTree);
+                Console.WriteLine("-- BUILD SUCCESSFUL --");
             }
         }
 
