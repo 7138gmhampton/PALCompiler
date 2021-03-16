b5446b9c0aa626a71f011eb41d063f04219c00b7
7138gmhampton <0407435@abertay.ac.uk>
2021-03-16 19:38:11 +0000

    Add class summary for CS Generator class

--- PALCompiler/PALCSGenerator.cs
+++ PALCompiler/PALCSGenerator.cs
@@ -2,14 +2,15 @@
 
 namespace PALCompiler
 {
+    /// <summary>
+    /// Generates C# source from the syntax tree that has undergone semantic
+    /// analysis
+    /// </summary>
     partial class PALCSGenerator
     {
         public SyntaxNode syntax_tree;
 
-        internal PALCSGenerator(SyntaxNode syntax_tree)
-        {
-            this.syntax_tree = syntax_tree;
-        }
+        internal PALCSGenerator(SyntaxNode syntax_tree) { this.syntax_tree = syntax_tree; }
 
         internal string generate()
         {
