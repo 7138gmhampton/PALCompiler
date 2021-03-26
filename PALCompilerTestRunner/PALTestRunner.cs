84d27ee80fd656d575520bb84de624c4cb9f8856
7138gmhampton <0407435@abertay.ac.uk>
2021-03-26 18:11:11 +0000

    Set to run through each subdirectory

--- PALCompilerTestRunner/PALTestRunner.cs
+++ PALCompilerTestRunner/PALTestRunner.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,11 @@ namespace PALCompilerTestRunner
     {
         static void Main(string[] args)
         {
+            string[] suites = Directory.GetDirectories(@"./");
+
+            foreach (var suite in suites) {
+                Console.WriteLine("-- " + suite + "  --");
+            }
         }
     }
 }
